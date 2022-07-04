b09a5f0b42ee704729600712e7a6c1491ca3642c
ahmedkh19 <ahmed@thinkvolc.com>
2022-07-04 14:41:58 +0300

    =include style

--- functions.php
+++ functions.php
@@ -29,3 +29,14 @@ include( 'configure/acf.php' );
 if(is_admin()) {
 	include( 'configure/admin.php' );
 }
+
+function add_theme_scripts() {
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  
+  wp_enqueue_style( 'maincss', get_template_directory_uri() . '/assets/dist/css/main.css', array(), '1.1', 'all');
+    
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+      wp_enqueue_script( 'comment-reply' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
